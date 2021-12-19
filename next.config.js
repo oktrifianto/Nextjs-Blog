67f16edb8d7a3f16a4f12c014502e50bd32f2c6f
Rahmat <oktrifianto@gmail.com>
2021-12-19 14:58:22 +0700

    Add: create next.config.js for images setting

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/**
+ * how to use?
+ * example:
+ * src="https://images.pexels.com/photos/792199/pexels-photo-792199.jpeg?crop=entropy&cs=srgb&dl=pexels-nao-triponez-792199.jpg&fit=crop&fm=jpg&h=427&w=640"
+ */
+module.exports = {
+  images: {
+    domains: ['images.pexels.com'],
+  },
+}
