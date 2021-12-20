ed7063af18f21c7271a7550df2d364efd382045b
Rahmat <oktrifianto@gmail.com>
2021-12-20 19:43:37 +0700

    Update: change CSS and date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,7 +6,9 @@
 
 import Layout from '../../components/layout'
 import Head from 'next/head'
+import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -31,13 +33,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title} - Blog</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
